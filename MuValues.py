c53b2b2949cd05617702962e98698c03f4723ad8
kraken5567 <64876071+kraken5567@users.noreply.github.com>
2024-10-28 15:36:04 -0400

    Add files via upload

--- MuValues.py
+++ MuValues.py
@@ -5,8 +5,8 @@ import math
 
 # Math assumptions
 g = 9.81 # m/(s**2)
-mu = 0 # radians
-path_radius = 0.25 # meters
+theta = 0 # radians
+path_radius = 0.017 # meters
 
 # prefunction calculations
 distance = 2 * math.pi * path_radius
@@ -24,6 +24,8 @@ for file in DataSets_fldr:
         with open(f"DataSet_values\\{valueSet}.json","r") as setValues:
             fileValue = json.load(setValues)
 
+        print(f"\nWheel Name: {valueSet}")
+
         #
         # ANGULAR VELOCITY
         #
@@ -32,26 +34,34 @@ for file in DataSets_fldr:
         [w_x , w_y, w_z] = [csv["Angular velocity X(°/s)"].mean(), csv["Angular velocity Y(°/s)"].mean(), csv["Angular velocity Z(°/s)"].mean()]
         ang_velocity = math.sqrt(w_x**2 + w_y**2 + w_z**2)
 
-        # print(f"{valueSet} : {[w_x , w_y, w_z]}") # Remove "#" for debug!
+        print(f"Magnitude of Angular Velocity = {ang_velocity}")
+
+        #print(f"{valueSet} : {[w_x , w_y, w_z]}") # Remove "#" for debug!
 
         #
         # ROLLING FRICTION
         #
 
-        mu_rA = g * math.sin(mu)
-        mu_rB = (ang_velocity * fileValue["radius"])**2; mu_rB = mu_rB/(2 * distance)
-        mu_rC = g * math.cos(mu)
+        radius = fileValue["radius"]
+        print(f"Wheel Radius: {radius}")
 
-        # print(f" mu_rA -> {mu_rA} \n mu_rB -> {mu_rB} \n mu_rC -> {mu_rC} \n ") # Remove "#" for debug!
+        # print(f" ({g * math.sin(theta)}) - ({(ang_velocity * radius)**2}/{2 * distance}) \n --------------- \n ({g * math.cos(theta)})") # Remove "#" for debug!
 
-        mu_r = (mu_rA - mu_rB) / mu_rC # calculation
-        mu_r = abs(mu_r) # get the ratio (0 <= mu <= 1)
+        mu_r = ((g*math.sin(theta)) - ((ang_velocity * radius)**2)/2*distance)/(g*math.cos(theta));# calculation
+        mu_r = abs(mu_r) # get the ratio (0 < mu < 1)
+
+        print(f"Rolling Friction: {mu_r}")
 
         calcualtion_dictonary[f"{valueSet}"] = mu_r
 
 #
 # Write Results into a JSON
 #
+
+print("\n NOTE! Lower Rolling Friction is Better \n")
+print("Results (Also in \"results.json\"): ")
+for key in calcualtion_dictonary.keys():
+    print(f"{key} : {calcualtion_dictonary[key]}")
 
 with open("results.json","w") as results:
     json.dump(calcualtion_dictonary,results,indent=1)
